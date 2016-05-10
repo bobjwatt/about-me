b5f0f03e3860136f13c4e18b55f6904ea31b4756
Bob <bobjwatt@gmail.com>
2016-05-10 14:29:28 -0700

    start

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-//this is a comment
+var userName = prompt('Hi, thanks for visiting my page!! What is your name');
+alert('Nice to meet you, ' + userName + '. I am going to ask some questions.');
+console.log('the user name is ' + userName + ' and he cool.');
+
+var response1 = prompt('Do I have 3 cats?').toLowerCase();
+var answer1 = 'yes';
+
+if (the user is correct); {
+  tell them they got it right
+} else {
+  tell them they got it wrong
+}
+if (reponse1 === answer1 |\ response1 === answer1[0]{
+  alert('you are correct, ' + userName = '! My cats are Fluffy, Homey, and Buster');
+} else {
+  alert('Nope. I really do have 3 cats.');
+}
