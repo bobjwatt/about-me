94f5f78f407fe649a167d5e1c6de863610613ee8
Bob <bobjwatt@gmail.com>
2016-05-10 21:00:28 -0700

    Grammer change in JS

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var userName = prompt('Hi, thanks for visiting my page!! What is your name?');
-alert('Nice to meet you, ' + userName + '. I am going to ask some questions.');
+alert('Nice to meet you, ' + userName + '. I am going to ask one question.');
 console.log('the user name is ' + userName + ' and he is the best potential boss ever!');
 
 var response1 = prompt('Do you really plan to hire me? Be honest.').toLowerCase();
@@ -8,5 +8,5 @@ var answer1 = 'yes';
 if (response1 === answer1 || response1 === answer1[0]) {
   alert('That is fantastic ' + userName + ', When do I start?');
 } else {
-  alert('Well thanks for nothing' + userName + ', I really do have better things to do you know!');
+  alert('Well thanks for nothing ' + userName + ', I really do have better things to do you know!');
 }
