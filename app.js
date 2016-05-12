a035ff0d02c9702a93db69da6ffec3073b491518
Bob <bobjwatt@gmail.com>
2016-05-12 06:07:46 -0700

    at-sevth-question

--- app.js
+++ app.js
@@ -1,12 +1,57 @@
 var userName = prompt('Hi, thanks for visiting my page!! What is your name?');
-alert('Nice to meet you, ' + userName + '. I am going to ask one question.');
+alert('Nice to meet you, ' + userName + '. If it\'s okay I would like you to find out more about me by asking me a few questions. No pressure, give it your best shot.');
 console.log('the user name is ' + userName + ' and he is the best potential boss ever!');
 
-var response1 = prompt('Do you really plan to hire me? Be honest.').toLowerCase();
+/*var response1 = prompt('Do I have 2 kids?').toLowerCase();
 var answer1 = 'yes';
 
 if (response1 === answer1 || response1 === answer1[0]) {
-  alert('That is fantastic ' + userName + ', When do I start?');
+  alert('That right ' + userName + ', I have a son named Gavin and a daughter named Ruby.');
 } else {
-  alert('Well thanks for nothing ' + userName + ', I really do have better things to do you know!');
+  alert('Yes it\'s true I\'m a family man ' + userName + ', I really do have two kids!');
 }
+
+var response1 = prompt('Do I live in Seattle?').toLowerCase();
+var answer1 = 'yes';
+
+if (response1 === answer1 || response1 === answer1[0]) {
+  alert('No ' + userName + ', afraid not.');
+} else {
+  alert('That\'s right ' + userName + ', I live in T-Town!');
+}
+
+var response1 = prompt('Do I have pets?').toLowerCase();
+var answer1 = 'yes';
+
+if (response1 === answer1 || response1 === answer1[0]) {
+  alert('I\'m sorry to inform you ' + userName + ', I do not.');
+} else {
+  alert('That\'s right' + userName + ', no pets for us. My son is highly allergic.');
+}
+
+var response1 = prompt('Do I really plan work here if I get hired?').toLowerCase();
+var answer1 = 'yes';
+
+if (response1 === answer1 || response1 === answer1[0]) {
+  alert('You got that right ' + userName + ', When do I start?');
+} else {
+  alert('Well ' + userName + ', I really do have better things to do than waste our time!');
+}*/
+
+/*alert('Okay, now I would like to play a guessing game. I will give you 4 chances to get it right.');
+var userAnswer = '3';//Question: this works only with string 3 not number 3
+for(var i = 0; i < 4; i++){
+  var guess = prompt('Guess how many classic cars I have restored?');
+  if (guess < userAnswer){
+    alert('Too low, try another guess.');
+  } else if (guess > userAnswer){
+    alert('Too high, guess again.');
+  } else if (guess === userAnswer){ //if I change userNumber to number 3, I have to chane this to a equal from strict equal??
+    alert('Yes! Congratulations - you guessed it!');
+    break;
+  } else {
+    alert('Alright ' + userName + ' Let\'s keep to numbers. Try it again.');//displayed even if guess correct. dosesn't promt another turn??
+  }
+}*/
+alert('Okay, now our 7th and final question. I\'ll give you 6 tries.');
+var userState = 'California, Oregon';//Mental road-block. I know I need an array and a for loop but I don't know where to start??Help!!!
